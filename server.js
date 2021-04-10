02b0f9756ba1ac182ee367683f6688bb8646db73
Shlomi Moreh <shlomi295@gmail.com>
2021-04-10 16:43:13 +1000

    Added mongodb module Connected to the db

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require("express"),
     app = express();
 
+
+//DB
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://dbuser2021:m7EeKkKON5miaG1A@eyesistantcluster.agziq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("eyesistantDb").collection("text");
+  // perform actions on the collection object
+  console.log('connected')
+  client.close();
+});
+
+
+
 var port = process.env.PORT || 8080;
 //this is my first comment to make sure it's committing to the git repo
 app.use(express.static(__dirname + '/public'));

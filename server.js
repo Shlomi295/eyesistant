7c664af379430d676d3336bdd7fa52414101159a
Shlomi Moreh <shlomi295@gmail.com>
2021-04-10 15:24:24 +1000

    init first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express"),
     app = express();
 
 var port = process.env.PORT || 8080;
-
+//this is my first comment to make sure it's committing to the git repo
 app.use(express.static(__dirname + '/public'));
 
 app.get("/sayHello", function (request, response) {
